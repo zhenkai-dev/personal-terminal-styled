3c39dd3dafa7592ccc3a6bf88096c46934314deb
Zhen Kai <zhenkai.dev@gmail.com>
2025-08-22 23:38:26 +0800

    add: vercel web analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from 'next'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata: Metadata = {
   title: 'Wong Zhen Kai - Full Stack Developer',
@@ -24,6 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <Analytics />
     </html>
   )
 }
